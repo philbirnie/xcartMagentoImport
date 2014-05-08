5ce23334958a26d4ad80144e63d08a4d6b8f0890
Phil Birnie <pbirnie@sbcadvertising.com>
2014-05-08 11:48:26 -0400

    Allows specification of database table when creating database instance.

--- lib/Importgen/DB.php
+++ lib/Importgen/DB.php
@@ -19,20 +19,25 @@ class DB
      * Convenience function for setting up database connection
      * @return DB Database connect or FALSE if could not connect
      */
-    public static function get()
+    public static function get($alt_db = null)
     {
-        $pdo = new DB();
+        $pdo = new DB($alt_db);
         return $pdo;
     }
 
-    public function __construct()
+    public function __construct($alt_db = null)
     {
+        if(!is_null($alt_db)) {
+            $db = $alt_db;
+        } else {
+            $db = DB_NAME;
+        }
         try{
             /**
              * Create new PDO Object
              * @var PDO
              */
-            $db_conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+            $db_conn = new PDO('mysql:host='.DB_HOST.';dbname='.$db, DB_USER, DB_PASSWORD);
             $this->conn = $db_conn;
         } catch (PDOException $e) {
             echo "Unable to connect to database; please check your database settings "  . $e->getMessage();
